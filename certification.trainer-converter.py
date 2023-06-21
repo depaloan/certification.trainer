9af051dd03bfa0d9c4b7ee42663e5621150b1789
Andrea de Palo <depalo.andrea@gmail.com>
2023-06-21 17:58:48 +0200

    add input file support

--- certification.trainer-converter.py
+++ certification.trainer-converter.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python
 
+import sys
+from pathlib import Path
+
+if (len(sys.argv) != 2):
+    print("Please provide input TXT file")
+    print("Usage: certification.trainer-converter.py MY_CERT.txt") 
+    sys.exit(1)
+
+file_path = Path(sys.argv[1])
+if (not file_path.is_file()):
+    print("File does not exists or user is not allowed to access it")
+    print("Usage: certification.trainer-converter.py MY_CERT.txt") 
+    sys.exit(1)
+
 begin_answers=0
 
-with open('file.txt', 'r', encoding='utf-8') as source:
+with open(file_path, 'r', encoding='utf-8') as source:
     for line in source:
         #print(line.strip())
         if ( ('Y. ' in line) or ('N. ' in line) ):

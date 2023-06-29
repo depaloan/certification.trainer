04bf7132058dff06e70ce9fe1515b201892c7d00
Andrea de Palo <depalo.andrea@gmail.com>
2023-06-29 17:29:56 +0200

    fix JSON generation

--- certification.trainer-converter.py
+++ certification.trainer-converter.py
@@ -17,20 +17,30 @@ if (not file_path.is_file()):
 begin_answers=0
 
 with open(file_path, 'r', encoding='utf-8') as source:
+    print('{') 
+    print('"questions": [') 
     for line in source:
-        #print(line.strip())
-        if ( ('Y. ' in line) or ('N. ' in line) ):
-            if not(begin_answers==0):
-                print(',')
-            print('"' + line.strip() + '"', end = '')
-            begin_answers=1
-        if ( not ('Y. ' in line) and not ('N. ' in line) and not(len(line) == 1) ):
-            print('{')
-            print('"question": "' + line.strip() + '",')
-            print('"answers": [')
-        if ( (len(line) == 1) and begin_answers==1):
-            print(']')
-            print('},') 
-            begin_answers=0
+        if ('-- ' in line):
+            #if ('-- certification_name: ' in line):
+            #    certification_name=line.split(': ')
+            #    print(certification_name[1])
+            #    #"certification_name": "Heavy Metal quiz",
+            pass
+        else:
+            if ( ('Y. ' in line) or ('N. ' in line) ):
+                if not(begin_answers==0):
+                    print(',')
+                print('"' + line.strip() + '"', end = '')
+                begin_answers=1
+            if ( not ('Y. ' in line) and not ('N. ' in line) and not(len(line) == 1) ):
+                print('{')
+                print('"question": "' + line.strip() + '",')
+                print('"answers": [')
+            if ( (len(line) == 1) and begin_answers==1):
+                print(']')
+                print('},') 
+                begin_answers=0
     print(']')
     print('}') 
+print(']')
+print('}') 
